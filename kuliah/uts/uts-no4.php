c0583e406218a0a4f19e4af11d70b0969bbef13e
GalangDhaffa <davagalang3456@gmail.com>
2024-05-19 19:44:25 +0700

    memberbarui

---
+++ kuliah/uts/uts-no4.php
@@ -0,0 +1,18 @@
+<?php 
+$hewan = array("kucing", "anjing", "monyet");
+
+echo $hewan[0];
+echo $hewan[1]; 
+echo $hewan[2]; 
+
+echo "<br>";
+
+$user = array(
+    "nama" => "Ucup Birjon",
+    "email" => "Ucupbjr@gmail.com",
+    "usia" => 19
+);
+
+echo $user["nama"];
+echo $user["email"];
+echo $user["usia"];
